1fc698eea7b5640084d5e1d5d93e0d66d3abd268
shiyuxiu <xiushiyu666@gmail.com>
2021-09-04 17:38:47 -0600

    update script.py

--- script.py
+++ script.py
@@ -1,2 +1,6 @@
+import urllib.request
 import requests
 print(requests.__version__)
+request=requests.get("https://raw.githubusercontent.com/xius666/404_Lab1/main/script.py") 
+print(request.text)
+urllib.request.urlretrieve("https://raw.githubusercontent.com/xius666/404_Lab1/main/script.py", "downloadfile.py")
