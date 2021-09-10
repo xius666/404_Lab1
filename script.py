a50191cb49d4766bc4a52a98a2053d3e5992baae
Shiyu Xiu <xiushiyu666@gmail.com>
2021-09-09 22:32:52 -0600

    update the curl google home page

--- script.py
+++ script.py
@@ -1,6 +1,11 @@
 import urllib.request
 import requests
 print(requests.__version__)
+request_google= requests.get("https://www.google.com")
+print("header of the google page:")
+print(request_google.headers)
+
+
 request=requests.get("https://raw.githubusercontent.com/xius666/404_Lab1/main/script.py") 
 print(request.text)
 urllib.request.urlretrieve("https://raw.githubusercontent.com/xius666/404_Lab1/main/script.py", "downloadfile.py")
